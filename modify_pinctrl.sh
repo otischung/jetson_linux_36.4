9deb55362be3ba5e4032d4143f6cae0edc682cfb
otischung <p76124605@gs.ncku.edu.tw>
2024-12-31 16:08:33 +0800

    [REFACTOR] Move source files to folders

--- modify_pinctrl.sh
+++ modify_pinctrl.sh
@@ -2,5 +2,5 @@
 
 set -euo pipefail
 
-cp pinctrl-tegra.c Linux_for_Tegra/source/kernel/kernel-jammy-src/drivers/pinctrl/tegra/pinctrl-tegra.c
-cp pinctrl-tegra.h Linux_for_Tegra/source/kernel/kernel-jammy-src/drivers/pinctrl/tegra/pinctrl-tegra.h
+cp src/pinctrl-tegra.c Linux_for_Tegra/source/kernel/kernel-jammy-src/drivers/pinctrl/tegra/pinctrl-tegra.c
+cp src/pinctrl-tegra.h Linux_for_Tegra/source/kernel/kernel-jammy-src/drivers/pinctrl/tegra/pinctrl-tegra.h
